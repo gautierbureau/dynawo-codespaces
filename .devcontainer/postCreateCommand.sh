6b8ee17363278f75cfe8c72992728cc2657f0b07
Gautier Bureau <gautier.bureau@rte-france.com>
2023-07-03 09:57:59 +0200

    fix

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,18 +7,19 @@ export DYNAWO_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export DYNAWO_SRC_OPENMODELICA="$DYNAWO_HOME"/OpenModelica/Source
 export DYNAWO_INSTALL_OPENMODELICA=/opt/dynawo/OpenModelica
 export DYNAWO_THIRD_PARTY_INSTALL_DIR=/opt/dynawo
+export OPENMODELICAHOME=/opt/dynawo/OpenModelica
 
 export DYNAWO_LOCALE=en_GB
 export DYNAWO_RESULTS_SHOW=false
 export DYNAWO_BROWSER=lynx
 
-[ -z "$DYNAWO_NB_PROCESSORS_USED" ] && export DYNAWO_NB_PROCESSORS_USED=2
+[ -z "$DYNAWO_NB_PROCESSORS_USED" ] && export DYNAWO_NB_PROCESSORS_USED=1
 
 export DYNAWO_BUILD_TYPE=Debug
 
 export DYNAWO_PYTHON_COMMAND=python3
 
-export libiidm_DIR=/opt/dynawo/share/cmake
+export LC_ALL=C; unset LANGUAGE
 export CMAKE_PREFIX_PATH=/opt/dynawo/cmake/:/opt/dynawo/share/cmake
 
 "$DYNAWO_HOME"/util/envDynawo.sh $@' > myEnvDynawo.sh
@@ -41,3 +42,8 @@ ln -s /opt/dynawo/ /opt/dynawo/suitesparse
 ln -s /opt/dynawo/ /opt/dynawo/sundials
 ln -s /opt/dynawo/ /opt/dynawo/libxml2
 ln -s /opt/dynawo/ /opt/dynawo/libzip
+cp /opt/dynawo/lib/libl* /opt/dynawo/OpenModelica/lib/x86_64-linux-gnu/omc
+cp /opt/dynawo/lib/libgfortran.so.3 /opt/dynawo/OpenModelica/lib/x86_64-linux-gnu/omc
+sed -i '17a\ \ \ \ \"cmake.parallelJobs": "1",' .vscode/settings.json
+sed -i 's|\${workspaceFolder}/OpenModelica/Install|/opt/dynawo/OpenModelica|' .vscode/settings.json
+sed -i 's/@CMAKE_GENERATOR@/Unix Makefiles/' dynawo/sources/ModelicaCompiler/PreloadCache.cmake.in
